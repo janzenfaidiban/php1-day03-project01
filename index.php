a67c010f39eaa49827a7f1ef1e51f87b380a4cb9
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-05-30 19:41:21 +0900

    simpan pertama

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $judul = 'SaCode - PHP Batch I Day 03';
+  $isi = 'Belajar PHP untuk pemula di SaCode.';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $judul; ?></title>
+  </head>
+  <body>
+
+    <h1><?php print $judul; ?></h1>
+    <p><?php echo $isi; ?></p>
+
+  </body>
+</html>
